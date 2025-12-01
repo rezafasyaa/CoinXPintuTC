541169f1abf28ff6a79260cab0871497e59eff3c
Reza Fasya <muhamadfasya@gmail.com>
2025-12-01 20:59:47 +0700

    Update and rename mart_trading_concentration.sql to mart_trade_concentration.sql

--- models/mart/mart_trade_concentration.sql
+++ models/mart/mart_trade_concentration.sql
@@ -1,38 +1,43 @@
 WITH monthly_token_record AS (
   SELECT
-    token_id
-    ,DATE_TRUNC('month',trade_created_time) AS month_date
+    DATE_TRUNC('month',trade_created_time) AS month_date  
+    ,token_id
     ,side
-    ,COUNT(quantity) AS shares_count
+    ,COUNT(trade_id) AS shares_freq
+    ,COUNT(quantity) AS shares_qty
     ,SUM(amount_usd) AS shares_amount
   FROM fact_trades
   WHERE 1=1
     and status = 'FILLED'
-  GROUP BY 1,2
+  GROUP BY 1,2,3
 )
 
 ,monthly_record AS (
   SELECT 
     DATE_TRUNC('month',trade_created_time) AS month_date
     ,side
-    ,COUNT(quantity) AS total_shares_count
+    ,COUNT(trade_id) AS total_shares_freq
+    ,COUNT(quantity) AS total_shares_qty
     ,SUM(amount_usd) AS total_shares_amount
   FROM fact_trades
   WHERE 1=1
     and status = 'FILLED'
-  GROUP BY 1
+  GROUP BY 1,2
 )
 
 SELECT
   mtr.month_date
   ,mtr.token_id
   ,mtr.side
-  ,mtr.shares_count
-  ,mr.total_shares_count
-  ,(mtr.shares_count/mr.total_shares_count) AS shares_pct
+  ,mtr.shares_freq
+  ,mr.total_shares_freq
+  ,(mtr.shares_freq/mr.total_shares_freq) AS shares_freq_pct
+  ,mtr.shares_qty
+  ,mr.total_shares_qty
+  ,(mtr.shares_qty/mr.total_shares_qty) AS shares_qty_pct
   ,mtr.shares_amount
   ,mr.total_shares_amount
-  ,(mtr.shares_amount/mr.total_shares_amount) AS amount_pct
+  ,(mtr.shares_amount/mr.total_shares_amount) AS shares_amount_pct
 FROM monthly_token_record mtr
 JOIN monthly_record mr
   ON mr.month_date = mtr.month_date
