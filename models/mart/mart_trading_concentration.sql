1b5ab49991fe3ae9f3c6c1607bda7da1c5620b39
Reza Fasya <muhamadfasya@gmail.com>
2025-12-01 15:41:33 +0700

    Update mart_trading_concentration.sql

--- models/mart/mart_trading_concentration.sql
+++ models/mart/mart_trading_concentration.sql
@@ -1,32 +1,36 @@
-WITH monthly_token_amount AS (
+WITH monthly_token_record AS (
   SELECT
     token_id
     ,DATE_TRUNC('month',trade_created_time) AS month_date
     ,side
-    ,count(trade_id) AS trade_count
+    ,COUNT(trade_id) AS trade_count
     ,SUM(amount_usd) AS trade_amount
   FROM fact_trades
   GROUP BY 1,2
 )
 
-,monthly_amount AS (
+,monthly_record AS (
   SELECT 
     DATE_TRUNC('month',trade_created_time) AS month_date
     ,side
-    ,count(trade_id) AS trade_count
-    ,SUM(amount_usd) AS trade_amount
+    ,COUNT(trade_id) AS total_trade_count
+    ,SUM(amount_usd) AS total_trade_amount
   FROM fact_trades
   GROUP BY 1
 )
 
 SELECT
-  mta.month_date
-  ,mta.token_id
-  ,mta.token_amount
-  ,ma.total_amount
-  ,(mta.token_amount / ma.total_amount) AS perc_volume
-FROM monthly_token_amount mta
-JOIN monthly_amount ma
-  ON ma.month_date = mta.month_date
-  AND ma.side = mta.side
+  mtr.month_date
+  ,mtr.token_id
+  ,mtr.side
+  ,mtr.trade_count
+  ,mr.total_trade_count
+  ,(mtr.trade_count/mr.total_trade_count) AS count_pct
+  ,mtr.trade_amount
+  ,mr.total_trade_amount
+  ,(mtr.trade_amount/mr.total_trade_amount) AS amount_pct
+FROM monthly_token_record mtr
+JOIN monthly_record mr
+  ON mr.month_date = mtr.month_date
+  AND mr.side = mtr.side
 ORDER BY 1
